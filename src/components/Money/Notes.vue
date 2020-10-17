71691ad45201894354223882ccfc32264e4438d5
lb66 <807242867@qq.com>
2020-10-17 23:26:09 +0800

    note组件优化

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,37 +1,34 @@
 <template>
-  <div>
-    <label class="notes" :class="`${classPrefix}-form` ">
-      <span class="name">{{filedName}}</span>
-      <template v-if="type==='date'">
-        <input
-          :type="type || 'text'"
-          :placeholder="placeholder"
-          :value="iso(value)"
-          @input="$emit('update:value', $event.target.value)"
-        />
-      </template>
-      <template v-else>
-        <input
-          :type="type || 'text'"
-          :placeholder="placeholder"
-          :value="value"
-          @input="$emit('update:value', $event.target.value)"
-        />
-      </template>
-    </label>
-  </div>
-</template> 
+<div>
+  <label class="notes" :class="`${classPrefix}-form` ">
 
-<script lang='ts'>
-import { Vue, Component, Prop } from "vue-property-decorator";
+    <template v-if="type==='date'">
+      <input :type="type || 'text'" :placeholder="placeholder" :value="iso(value)" @input="$emit('update:value', $event.target.value)" />
+    </template>
+    <template v-else>
+      <span class="name">{{filedName}}</span>
+      <input :type="type || 'text'" :placeholder="placeholder" :value="value" @input="$emit('update:value', $event.target.value)" />
+    </template>
+  </label>
+</div>
+</template>
+
+<script lang="ts">
+import {
+  Vue,
+  Component,
+  Prop
+} from "vue-property-decorator";
 import dayjs from "dayjs";
 @Component
 export default class Notes extends Vue {
-  @Prop({ default: "" }) value!: string;
-  @Prop(String) filedName?: string;
-  @Prop(String) placeholder?: string;
-  @Prop(String) type?: string;
-  @Prop(String) classPrefix?: string;
+  @Prop({
+    default: ""
+  }) value!: string;
+  @Prop(String) filedName? : string;
+  @Prop(String) placeholder? : string;
+  @Prop(String) type? : string;
+  @Prop(String) classPrefix? : string;
 
   iso(isoString: string) {
     return dayjs(isoString).format("YYYY-MM-DD");
@@ -45,11 +42,15 @@ export default class Notes extends Vue {
   padding-left: 16px;
   display: flex;
   align-items: center;
+
   .name {
     padding-right: 16px;
+    width: 75px;
   }
+
   input {
     height: 64px;
+    width: 100%;
     background: transparent;
     border: none;
     padding-right: 16px;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,23 +1,31 @@
 <template>
-  <Layout classPrefix="layout" :style="{height:h+'px'}">
-    <NumberPad :value.sync="record.amount" @submit="saveRecord" />
-    <Tabs :dataSource="typeList" :value.sync="record.type" />
-    <Notes :value.sync="record.notes" filedName="备注" placeholder="点击写备注..." />
-    <Tags :value.sync="record.tags" />
-    <Notes :value.sync="record.createAt" type="date" classPrefix="date" filedName="小B记账" />
-    <img :src="QR" class="qr" @click="hidden(isShow)" v-show="isShow" />
-  </Layout>
+<Layout classPrefix="layout" :style="{height:h+'px'}">
+  <NumberPad :value.sync="record.amount" @submit="saveRecord" />
+  <Tabs :dataSource="typeList" :value.sync="record.type" />
+  <Notes :value.sync="record.notes" filedName="备注" placeholder="你要默默的记账，然后惊艳所有人" />
+  <Tags :value.sync="record.tags" />
+  <Notes :value.sync="record.createAt" type="date" classPrefix="date" />
+  <img :src="QR" class="qr" @click="hidden(isShow)" v-show="isShow" />
+</Layout>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import NumberPad from "@/components/Money/NumberPad.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
 import Tabs from "@/components/Tabs.vue";
-import { Vue, Component } from "vue-property-decorator";
+import {
+  Vue,
+  Component
+} from "vue-property-decorator";
 
 @Component({
-  components: { Tags, Notes, NumberPad, Tabs },
+  components: {
+    Tags,
+    Notes,
+    NumberPad,
+    Tabs
+  },
   computed: {
     recordList() {
       return this.$store.state.recordList;
@@ -26,9 +34,14 @@ import { Vue, Component } from "vue-property-decorator";
 })
 export default class Money extends Vue {
   h = document.body.clientHeight;
-  typeList = [
-    { text: "支出", value: "-" },
-    { text: "收入", value: "+" },
+  typeList = [{
+      text: "支出",
+      value: "-"
+    },
+    {
+      text: "收入",
+      value: "+"
+    },
   ];
   record: RecordItem = {
     tags: [],
@@ -64,22 +77,27 @@ export default class Money extends Vue {
   flex-direction: column-reverse;
   margin: 0 0 55px 0;
 }
+
 ::v-deep .date-form {
   background: #a6b8bd;
+
   .name {
     flex-grow: 1;
     font-weight: bold;
   }
+
   input {
     height: 44px;
   }
 }
+
 .qr {
   position: fixed;
   width: 180px;
   top: 7%;
   right: 5%;
 }
+
 @media screen and (max-width: 480px) {
   .qr {
     display: none;
